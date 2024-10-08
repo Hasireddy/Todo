0f4e9dc442180cd8f6f9cf1d26b692c60290199b
Hasireddy <malikireddy.uma@gmail.com>
2024-10-08 14:10:19 +0200

    modified remove view

--- Todo/MyToDo/views.py
+++ Todo/MyToDo/views.py
@@ -40,7 +40,17 @@ def remove_item(request,item_id):
     else:
         messages.error(request, "Invalid request method.")
     return redirect("todo:add")
+
     
+# When you need to access multiple fields of each Todo object, we use:
+# todos = Todo.objects.all()
+# for todo in todos:
+#     print(todo.id, todo.title, todo.details)  # Access all fields
 
-
-
+# When we want to retrieve only the specific field of Todo object or When you only need specific fields (like IDs) and not the full model instances.we use:
+#     or
+    
+# When you want a more efficient query that uses less memory, especially if you have a lot of fields in your model but only need a few of them.
+    
+#     todo_ids = Todo.objects.values_list('id',flat=True) #Gets a list of ids
+#     print(todo_ids)
