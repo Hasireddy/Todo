42f6cf26259944414f28faefbcd89e01eaddcf71
Hasireddy <malikireddy.uma@gmail.com>
2024-10-08 11:00:44 +0200

    remove item view created

--- Todo/MyToDo/urls.py
+++ Todo/MyToDo/urls.py
@@ -1,3 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from . views import index,remove
+
+
 app_name = "todo"
 
-urlpatterns = []
+urlpatterns = [
+    path("",index,name="todo"),
+    path("del/<str:item_id>",remove,name="delete"),
+    path("admin/",admin.site.urls)
+       
+]
